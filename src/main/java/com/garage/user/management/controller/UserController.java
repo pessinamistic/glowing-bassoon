aa887030e163c5cc28f535860d497480270c7091
Shashank Shekhar <53684221+pessinamistic@users.noreply.github.com>
2022-12-11 20:48:49 +0530

    new banner.txt new controller annotations

--- src/main/java/com/garage/user/management/controller/UserController.java
+++ src/main/java/com/garage/user/management/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.garage.user.management.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@CrossOrigin
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    
+
+
 }
